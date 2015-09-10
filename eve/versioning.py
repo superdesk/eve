6f1637a4b3f481e42bf43ed0b3f00f66ac7544d9
amagdas <magdasadrian@gmail.com>
2015-09-10 15:51:00 +0300

    flake8 fixes

--- eve/versioning.py
+++ eve/versioning.py
@@ -139,8 +139,9 @@ def insert_versioning_documents(resource, documents):
             versioned_documents.append(ver_doc)
 
         # bulk insert
-        versionable_resource_collection_name = resource_def['datasource']['source'] + app.config['VERSIONS']
-        app.data.insert(versionable_resource_collection_name, versioned_documents)
+        source = resource_def['datasource']['source']
+        versionable_resource_name = source + app.config['VERSIONS']
+        app.data.insert(versionable_resource_name, versioned_documents)
 
 
 def versioned_fields(resource_def):
